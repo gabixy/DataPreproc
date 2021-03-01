071b74dcdad45d9f4fba3986cacbd955f1732b33
Gabija <gabija.pppp@gmail.com>
2021-03-01 21:52:56 +0200

    Fixed and added some code

--- 1laboratorinis.R
+++ 1laboratorinis.R
@@ -1,22 +1,24 @@
+setwd("C:/Users/gabij/Desktop/Mokslai/4SEMESTRAS/Programavimas_viz/LAB1")
 
         ## 1 uzduotis
 
 # a) DUOMENU NUSKAITYMAS + BIBLIOTEKU ISSAUKIMAS
 
-setwd("C:/Users/gabij/Desktop/Mokslai/4SEMESTRAS/Programavimas_viz/LAB1")
-
 library(dplyr)
 library(lubridate)
 library(haven)
+library(tidyr)
 
 propinfo<-read.csv("Real_Property_Information.csv")
 
 # b) DUOMENU STRUKTURA
 
 dim(propinfo)
-str(propinfo[,30:50]) 
-#duomenys turi laukeliu, kurie nera NA, taciau duomenyse yra "  " 
-#yra tarpai su skirtingais ilgiais
+str(propinfo[,30:40]) 
+# parinkau tik 10 stulpeliu, kuriuose yra ivairaus tipo duomenu, kad nebutu spausdinami 80
+# nes tai butu labai daug, atsizvelgiant i tai, kad yra stulpeliu, kuriuose yra ilgi link'ai
+# duomenys turi laukeliu, kurie nera NA, taciau duomenyse yra "  " 
+# yra tarpai su skirtingais ilgiais
 
 # c) DUOMENU VALYMAS
 
@@ -27,24 +29,32 @@ propinfo[,charcols] <- sapply(propinfo[,charcols], trimws) #naikina tarpus
 # tusciu reiksmiu ("") vertimas i NA
 propinfo[propinfo==""]<-NA
 
-str(propinfo[,30:50]) 
+# nuliu vertimas i NA is skaiciaus tipo kintamuju
+propinfo[propinfo==0]<-NA
 
+str(propinfo[,30:40]) 
+
+
+# del failo aprasymo trukumo, kategorinius kintamuosius parinkau savo nuoziura
 # kategoriniu kintamuju priskyrimas
-factors<- c("WARD", "SECTION","ASSESSOR", "PERMHOME", "ASSESGRP", 
-            "NO_IMPRV", "EXMPTYPE", "USEGROUP", "SDATCODE", "DIST_ID",
-            "ZONECODE", "DISTSWCH", "AR_OWNER", "OWNER_ABBR", "EXMPCODE",
-            "STDIRPRE", "ST_TYPE", "SRVCCNTR", "YEAR_BUILD", "DEEDPAGE",
+factors<- c("PERMHOME", "ASSESGRP", 
+            "NO_IMPRV", "EXMPTYPE", "USEGROUP", "DIST_ID",
+            "DISTSWCH", "AR_OWNER", "OWNER_ABBR",
+            "STDIRPRE", "ST_TYPE", "SRVCCNTR",
             "SUBTYPE_GEODB", "VACIND", "RESPAGCY")
+
+# kintamajame NO_IMPRV yra tik "Y" (yes) ir NA, tad NA paverciamas i kintamaji N (no)
+propinfo[,"NO_IMPRV"][is.na(propinfo[,"NO_IMPRV"])]<-"N"
 
 propinfo[, factors] <- lapply(propinfo[, factors], as.factor)
 
 # char kintamuju priskyrimas
-characters<- c("OBJECTID", "BLDG_NO", "ZIP_CODE", "EXTD_ZIP","SPAN_NUM")
+characters<- c("OBJECTID", "ZIP_CODE", "EXTD_ZIP")
 propinfo[,characters]<-lapply(propinfo[,characters], as.character)
 
 # datu tipu priskyrimas
 propinfo$SALEDATE<-mdy(propinfo$SALEDATE)
-#kazkodel lubridate nenuskaito datos "1172021", tad paverciau i "11-07-2021"
+#kazkodel lubridate nenuskaito datos "1172021", tad paverciau i "11-7-2021"
 propinfo$LDATE<-sub("(.{2})(.*)","\\1-\\2", propinfo$LDATE)
 propinfo$LDATE<-sub("(.{4})(.*)","\\1-\\2", propinfo$LDATE)
 propinfo$LDATE<-dmy(propinfo$LDATE) 
@@ -58,7 +68,10 @@ sapply(propinfo, class)
 sapply(propinfo[,factors], table)
 
 # f) SKAICIAUS TIPO CHARAKTERISTIKOS
+
 numcols <- names(propinfo)[sapply(propinfo, is.numeric)]
+# pasalinamas year kintamasis is numcols, kad neskaiciuotu jam vidurkio, max, mix
+numcols <- numcols[-(numcols=="YEAR_BUILD")]
 
 Mean<-sapply(propinfo[,numcols], mean, na.rm = T)
 Max_val<-sapply(propinfo[,numcols], max, na.rm = T)
@@ -77,16 +90,32 @@ nacols<-names(propinfo)[nasum>0]
 print(nacols)
 
 # b) REIKSMIU SU >50 PROC NA PASALINIMAS
-propinfo1<-propinfo[,!(nasum>(0.5*(dim(propinfo)[1])))]
+# I) stulpeliai
+propinfo1 <- propinfo
+
+miss <- c()
+for(i in 1:ncol(propinfo1)) {
+        if(length(which(is.na(propinfo1[,i]))) > 0.5*nrow(propinfo1)) miss <- append(miss,i) 
+}
+propinfo1 <- propinfo1[,-miss]
+
+# II) eilutes
+miss2 <- c()
+for(i in 1:nrow(propinfo1)) {
+        if(length(which(is.na(propinfo1[i,]))) > 0.5*ncol(propinfo1)) miss2 <- append(miss2,i)
+}
+# atlikus komanda prie miss2 metama reiksme yra NULL, t.y. nera eiluciu su >50% NA.
 
 nasum1<-colSums(is.na(propinfo1))
-nacols1<-names(propinfo1)[nasum>0]
+nacols1<-names(propinfo1)[nasum1>0]
 
 setdiff(nacols,nacols1) #stulpeliai, kurie buvo pasalinti
 
 # c) KEITIMAS I VIDURKI PAGAL KATEGORINI KINTAMAJI
 propinfo2<-propinfo1
 numcols2 <- names(propinfo2)[sapply(propinfo2, is.numeric)]
+# pasalinamas year kintamasis is numcols
+numcols2 <- numcols2[-(numcols2=="YEAR_BUILD")]
 
 for (a in numcols2)
 {
@@ -99,24 +128,6 @@ for (a in numcols2)
         }
 }
         
-# data3<-propinfo2[,1]
-# for (stulp in numcols1[4])
-# {
-#         require(dplyr)
-#         mutated <- propinfo2 %>% 
-#                 group_by(USEGROUP) %>% 
-#                 mutate(!!stulp := case_when(is.na(stulp) ~ mean(stulp, na.rm=T), TRUE ~ as.double(stulp))) %>%
-#                 ungroup() %>% 
-#                 select(stulp)
-#         print(mutated[1,1]) 
-# }
-# mutated <- propinfo2 %>% 
-#         group_by(USEGROUP) %>% 
-#         mutate(!!"ASSESSOR" := case_when(is.na(ASSESSOR) ~ mean(ASSESSOR, na.rm=T), TRUE ~ as.double(ASSESSOR))) %>%
-#         ungroup() %>% 
-#         select(ASSESSOR)
-# 
-
         #3 UZDUOTIS
 
 propinfo3<- propinfo2 %>% mutate(day=day(SALEDATE), month=month(SALEDATE), year=year(SALEDATE))
@@ -126,7 +137,16 @@ head(propinfo3[,c("OBJECTID","SALEDATE", "day", "month", "year")], n=10)
 
 propinfo4<-propinfo3
 
-# a) GRUBIU ISSKIRCIU SALINIMAS
+# a) GRUBIU ISSKIRCIU RADIMAS
+
+par(mfrow=c(2,2))
+for (i in numcols2[1:4])
+{
+        boxplot(propinfo4[,i], xlab=i)
+}
+par(mfrow=c())
+
+# b) GRUBIU ISSKIRCIU SALINIMAS
 remove_outliers <- function(duom, x, na.rm = TRUE, ...) 
 {
         qnt <- quantile(duom[,x], probs=c(.25, .75), na.rm = na.rm, ...)
@@ -142,7 +162,7 @@ for (a in numcols2)
         propinfo4[,a]<-remove_outliers(propinfo4, a)
 }
 
-#palyginimui
+# palyginimui
 colSums(is.na(propinfo3[,numcols2]))
 colSums(is.na(propinfo4[,numcols2]))
 
@@ -153,28 +173,34 @@ colSums(is.na(propinfo4[,numcols2]))
 # Pavyzdys: Stulpeliai STATETAX ir CITY_TAX, turintys papildoma informacija city ir state.
 
         #6 UZDUOTIS
+# select funkcija parinkta tik todel, kad nespausdintu 53 stulpeliu (labai netvarkingai atrodo)
+# realus ats butu gaunami pasalinus sia funkcija is visu uzduoties punktu
 # a) top 10 city tax objektai
 propinfo4 %>% top_n(n=10, CITY_TAX) %>% select(OBJECTID, CITY_TAX)
 
 # b) deedpage yra daugiau uz vidutine verte pagal owner_1 irasus abeceles nuo galo tvarka;
-propinfo4 %>% select(DEEDPAGE,OWNER_1) %>% filter(DEEDPAGE > mean(DEEDPAGE, na.rm=T)) %>%
-        arrange(desc(OWNER_1)) %>% head(n=10)
+propinfo4 %>% filter(DEEDPAGE > mean(DEEDPAGE, na.rm=T)) %>%
+        arrange(desc(OWNER_1)) %>%
+        select(DEEDPAGE,OWNER_1) %>%
+        head(n=10)
 
 # c) objektai, kuriu lot_size pavadinime yra simbolis X, o statetax virsija vidurki
-propinfo4 %>% select(LOT_SIZE, STATETAX) %>% 
+propinfo4 %>%
         filter(grepl("X",LOT_SIZE) & STATETAX>mean(STATETAX, na.rm=T)) %>%
+        select(LOT_SIZE, STATETAX) %>% 
         head(n=10)
 
 # d) objektai, kuriu st_name kode simbolis C yra pirmas arba paskutinis, duomenis, be
-# visu kintamuju, kuriu pavadinime yra simboliai cur
-propinfo4 %>% select(ST_NAME) %>% 
-        filter((grepl("^C",ST_NAME)|grepl(".*C$",ST_NAME) ) & !grepl("cur", ST_NAME, ignore.case = T)) %>%
+# visu kintamuju, kuriu pavadinime yra simboliai curr
+propinfo4 %>%
+        filter(grepl("^C",ST_NAME)|grepl(".*C$",ST_NAME)) %>%
+        select(-contains("curr",ignore.case=T)) %>% select(OBJECTID, ST_NAME) %>%
         head(10)
 
 # e) atskiri duomenu rinkiniai pagal kintamojo st_type kategorijas
 for (lev in levels(propinfo4$ST_TYPE))
 {
-       print(propinfo4 %>% select(OBJECTID, ST_TYPE) %>% filter(ST_TYPE==lev) %>% head(n=10))
+       print(propinfo4 %>% select(OBJECTID, ST_TYPE) %>% filter(ST_TYPE==lev) %>% head(n=2))
 }
         #7 UZDUOTIS
 
